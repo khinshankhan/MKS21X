cc505c75d96e2b9e7e40c8db7116c4b36ed3b186
Khinshan Khan <kkhan@stuy.edu>
2016-12-03 12:30:56 -0500

    First Prototype of SuperArrayIterator

--- 02SuperArrayIterator/SuperArrayIterator.java
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -1,2 +1,25 @@
 import java.util.*;
-public class SuperArrayIterator implements Iterator<String>{
+public class SuperArrayIterator implements Iterable<String>{
+    int start,end;
+    public SuperArrayIterator(int start, int end){
+    this.start = start;
+    this.end = end;
+  }
+    public Integer next(){
+    if(hasNext()){
+      start++;
+      return start-1;
+    }else{
+      throw new NoSuchElementException();
+    }
+  }
+
+  public boolean hasNext(){
+    return start < end;
+  }
+
+  public void remove(){
+    throw new UnsupportedOperationException();
+  }
+
+}
