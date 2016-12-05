12eb50699c2d76aa792891a622022b9a740dc376
Khinshan Khan <kkhan@stuy.edu>
2016-12-05 18:25:37 -0500

    First done

--- isDone/isDone.java
+++ isDone/isDone.java
@@ -4,6 +4,18 @@ public boolean isDone(){
   for(int i=1; i<16;i++){ 
     nums.add(i);
   }
+  for(int r=0, i=0; r <side; r++, i++){
+      for(int c=0; c <side; c++){
+	  if(values[r][c]==nums.get(i)){
+	      nums.remove(i);
+	      i++;
+	  }
+	  else if(values[r][c]==0){
+	      i++;
+	  }
+      }
+  }
+  return nums.size()==0;
 }
   
   
