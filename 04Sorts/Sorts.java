265349b7e655abed948387d4fbe1572cb35a2174
Khinshan Khan <kkhan@stuy.edu>
2016-12-13 20:48:48 -0500

    comments added

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -23,6 +23,10 @@ public class Sorts{
 	}
     }
 
+    /**Bubble sort of an int array. 
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
     public static void bubbleSort(int[] data){
 	int min;
 	for(int i=0; i < data.length; i++){
