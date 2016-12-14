958b3f652c0e4a8590b51c21bbb89a9dc6cc8285
Khinshan Khan <kkhan@stuy.edu>
2016-12-13 20:14:53 -0500

    done with selection sort and left test case within the file

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -9,9 +9,25 @@ public class Sorts{
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
-  public static void selectionSort(int[] data){
-      for(int i = 0; i < data.length - 1; i++){
-	  int min = i;
-	  for(int index = i; index < data.length; index++){
-	      if(data[min] < data[index])
+    public static void selectionSort(int[] data){
+	for(int i = 0; i < data.length - 1; i++){
+	    int min = i;
+	    for(int index = i + 1; index < data.length; index++){
+		if(data[min] > data[index]){
+		    min = index;
+		}
+	    }
+	    int previous = data[i];
+	    data[i] = data[min];
+	    data[min] = previous;
+	}
+    }
+    public static void main(String[] a){
+	int[] ary = {599, 84, 72, 1, 21};
+	Sorts.selectionSort(ary);
+	for(int i = 0; i < ary.length; i++){
+	    System.out.print(ary[i] + ",");
+	}
+    }
+}
 	      
